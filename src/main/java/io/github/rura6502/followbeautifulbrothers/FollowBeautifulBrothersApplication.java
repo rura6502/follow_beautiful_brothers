98378e4234c9554178262d895cecc0bd351e2208
jongseong woo <rura6502@hotmail.com>
2020-12-16 23:35:46 +0900

    feat: 스프링 프로젝트 초기화

---
+++ src/main/java/io/github/rura6502/followbeautifulbrothers/FollowBeautifulBrothersApplication.java
@@ -0,0 +1,13 @@
+package io.github.rura6502.followbeautifulbrothers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowBeautifulBrothersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowBeautifulBrothersApplication.class, args);
+	}
+
+}
